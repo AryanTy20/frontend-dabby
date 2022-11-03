e9e7ed9930dbd180d843e756cb338ccff882b945
AryanTy20 <devty007@gmail.com>
2022-11-04 00:17:37 +0530

    refactor: protect roues use user context user

--- src/layout/protectedRoute.tsx
+++ src/layout/protectedRoute.tsx
@@ -3,6 +3,7 @@ import { useUserContext } from "../hook/useUserContext";
 
 const protectedRoute = () => {
   const { user, setUser } = useUserContext();
+
   return user ? <Outlet /> : <Navigate to={"/login"} replace={true} />;
 };
 
