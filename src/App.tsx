fa360e02a1ec8d2abde18c54414ba0ec3a6cbbfd
AryanTy20 <devty007@gmail.com>
2022-11-03 20:04:42 +0530

    feat: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Register } from "./components";
+
+function App() {
+  return (
+    <>
+      <Register />
+      <nav></nav>
+      <main></main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
