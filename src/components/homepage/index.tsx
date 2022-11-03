76da9ac88c781739294febdb6fbc330e646d75d8
AryanTy20 <devty007@gmail.com>
2022-11-04 00:19:41 +0530

    fix: loaclstorage hooks functions name changed

--- src/components/homepage/index.tsx
+++ src/components/homepage/index.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useLocalStorage from "../../hook/useLocalStorage";
 import "./style.scss";
 
 const HomePage = () => {
+  const [setLocalUser, getLocalUser, deleteLocalUser] = useLocalStorage();
+  useEffect(() => {}, []);
+
   return <div>HomePage</div>;
 };
 
