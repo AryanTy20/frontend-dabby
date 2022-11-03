c4ad7cac47c7c6ab5c425dacb80e1115dfd94069
AryanTy20 <devty007@gmail.com>
2022-11-03 21:41:45 +0530

    feat: error boundary added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./layout";
+import { ErrorBoundary } from "react-error-boundary";
 import ProtectedRoute from "./layout/protectedRoute";
+import ErrorFallback from "./components/ErrorBoundary";
 const Register = React.lazy(() => import("./components/register"));
 const Login = React.lazy(() => import("./components/login"));
 const HomePage = React.lazy(() => import("./components/homepage"));
@@ -9,18 +11,23 @@ const HomePage = React.lazy(() => import("./components/homepage"));
 function App() {
   return (
     <>
-      <Suspense fallback={<h1>Loading</h1>}>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route element={<ProtectedRoute />}>
-              <Route path="/" element={<HomePage />} />
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={() => location.reload()}
+      >
+        <Suspense fallback={<h1>Loading</h1>}>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route element={<ProtectedRoute />}>
+                <Route path="/" element={<HomePage />} />
+              </Route>
+              <Route path="*" element={<h1>Page Not Found</h1>} />
             </Route>
-            <Route path="*" element={<h1>Page Not Found</h1>} />
-          </Route>
-        </Routes>
-      </Suspense>
+          </Routes>
+        </Suspense>
+      </ErrorBoundary>
     </>
   );
 }
