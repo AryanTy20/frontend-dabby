a2b175f746d90ce9e2d7c0473fc5d97a1e99ba8f
AryanTy20 <devty007@gmail.com>
2022-11-03 22:54:17 +0530

    feat: register form summit Eventhandler added

--- src/components/register/index.tsx
+++ src/components/register/index.tsx
@@ -2,7 +2,29 @@ import React from "react";
 import "./style.scss";
 
 const Register = () => {
-  return <div>Register</div>;
+  const registerHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const data = Object.fromEntries(formData);
+  };
+
+  return (
+    <section className="register">
+      <div className="register-box">
+        <h1>Register</h1>
+        <form onSubmit={registerHandler}>
+          <input type="text" name="username" placeholder="Username" />
+          <input type="text" name="password" placeholder="Password" />
+          <input
+            type="text"
+            name="repeatPassword"
+            placeholder="Repeat Password"
+          />
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Register;
