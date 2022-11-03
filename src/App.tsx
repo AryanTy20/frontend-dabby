be65a926ac393b438810207bd1427ebadb63e62c
AryanTy20 <devty007@gmail.com>
2022-11-03 20:30:27 +0530

    feat: layout added to route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+import Layout from "./layout";
 const Register = React.lazy(() => import("./components/register"));
 const Login = React.lazy(() => import("./components/login"));
 const HomePage = React.lazy(() => import("./components/homepage"));
@@ -9,11 +10,12 @@ function App() {
     <>
       <Suspense fallback={<h1>Loading</h1>}>
         <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-
-          <Route path="/" element={<HomePage />} />
-          <Route path="*" element={<h1>Page Not Found</h1>} />
+          <Route path="/" element={<Layout />}>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<h1>Page Not Found</h1>} />
+          </Route>
         </Routes>
       </Suspense>
     </>
