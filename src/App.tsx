5ea980297c134ccd88e4b89b2ad05c6ab001922c
AryanTy20 <devty007@gmail.com>
2022-11-03 20:23:46 +0530

    feat: components files and routes setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { Register } from "./components";
+import React, { Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+const Register = React.lazy(() => import("./components/register"));
+const Login = React.lazy(() => import("./components/login"));
+const HomePage = React.lazy(() => import("./components/homepage"));
 
 function App() {
   return (
     <>
-      <Register />
-      <nav></nav>
-      <main></main>
-      <footer></footer>
+      <Suspense fallback={<h1>Loading</h1>}>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
