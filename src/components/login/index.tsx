5cd3fef2ff5aaca01674c5dc955e2d8494dcb307
AryanTy20 <devty007@gmail.com>
2022-11-04 10:36:04 +0530

    feat: redirect already logged user

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,8 +1,57 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { customAxios } from "../../axios";
+import { useUserContext } from "../../hook/useUserContext";
+import { useNavigate } from "react-router-dom";
 import "./style.scss";
 
 const Login = () => {
-  return <div>Login</div>;
+  const { user, setUser } = useUserContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const alreadyLogged = async () => {
+      try {
+        const refreshUser = await customAxios("/auth/refreshuser");
+        if (refreshUser) navigate("/", { replace: true });
+      } catch (err) {
+        if (axios.isAxiosError(err) && err.message) {
+          console.log(err.message);
+        }
+      }
+    };
+
+    alreadyLogged();
+  }, []);
+
+  const loginHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const payload = Object.fromEntries(formData);
+    try {
+      const res = await customAxios.post("/auth/login", payload);
+      setUser(res.data.user);
+      navigate("/", { replace: true });
+    } catch (err) {
+      if (axios.isAxiosError(err) && err.message) {
+        console.log(err.message);
+      }
+    }
+  };
+
+  return (
+    <section className="login">
+      <div className="login-box">
+        <h1>Welcome Back !</h1>
+        <h2>Login</h2>
+        <form onSubmit={loginHandler}>
+          <input type="text" name="username" placeholder="Username" />
+          <input type="password" name="password" placeholder="Password" />
+          <input type="submit" value="Login" />
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
