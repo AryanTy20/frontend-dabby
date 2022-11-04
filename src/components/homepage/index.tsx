aed5a45a7b66a6712033c05f4d20579c4ca611ae
AryanTy20 <devty007@gmail.com>
2022-11-04 10:37:03 +0530

    refactor: removed localstorage hook

--- src/components/homepage/index.tsx
+++ src/components/homepage/index.tsx
@@ -1,13 +1,7 @@
 import React, { useEffect } from "react";
-import useLocalStorage from "../../hook/useLocalStorage";
 import "./style.scss";
 
 const HomePage = () => {
-  const { setLocalUser, getLocalUser, deleteLocalUser } = useLocalStorage();
-  useEffect(() => {
-    const user = getLocalUser();
-  }, []);
-
   return <div>HomePage</div>;
 };
 
