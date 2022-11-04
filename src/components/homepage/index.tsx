0f33667de62c0d2ba1bcd310d8d7adb20df8b644
AryanTy20 <devty007@gmail.com>
2022-11-04 22:04:02 +0530

    feat: serach in server funtionality added

--- src/components/homepage/index.tsx
+++ src/components/homepage/index.tsx
@@ -15,12 +15,14 @@ const btns = ["Get all Img", "Upload Image", "Search Img by Name"];
 const HomePage = () => {
   const [allImages, setAllImages] = useState<allImgType>([]);
   const [copyImages, setcopyImages] = useState<allImgType>([]);
+  const [serverSearchedImg, setServerSearchedImg] = useState<allImgType>([]);
   const [activeTab, setActiveTab] = useState({
     allImg: true,
     uploadImg: false,
     searchImg: false,
   });
   const [searchKey, setSearchKey] = useState("");
+  const [serverSearchKey, setServerSearchKey] = useState("");
   const [activeButton, setActiveButton] = useState(0);
   const [image, setImage] = useState<string | undefined>("");
   const [files, setFiles] = useState<fileType>(null);
@@ -125,11 +127,22 @@ const HomePage = () => {
     setAllImages(allImages.filter((el) => el.name === searchKey));
   };
 
+  const searchInServer = () => {
+    const getData = async () => {
+      const res = await customAxios("/user/search?name=mobile");
+      setServerSearchedImg(res.data);
+    };
+    getData();
+  };
+
   useEffect(() => {
-    if (searchKey === "") {
+    if (searchKey === "" && !searchInServer) {
       setAllImages(copyImages);
     }
-  }, [searchKey]);
+    if (serverSearchKey === "" && !searchKey) {
+      setServerSearchedImg(null);
+    }
+  }, [searchKey, serverSearchKey]);
 
   return (
     <>
@@ -217,7 +230,33 @@ const HomePage = () => {
                 </div>
               </div>
             )}
-            {activeTab.searchImg && <div className="allimg">allimg</div>}
+            {activeTab.searchImg && (
+              <div className="searchImg">
+                <h1 className="text-center h4">Search img in server</h1>
+                <div className="input-box ">
+                  <input
+                    type="search"
+                    className="form-control"
+                    value={serverSearchKey}
+                    onChange={(e) => setServerSearchKey(e.target.value)}
+                    placeholder={"search by name"}
+                  />
+                  <button
+                    className="btn btn-info text-white mx-1 my-2"
+                    onClick={searchInServer}
+                  >
+                    search
+                  </button>
+                </div>
+                <div className="img-container">
+                  {serverSearchedImg?.map((item, i) => (
+                    <div className="box" key={i}>
+                      <img src={item?.image} alt={item.name} />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
           </div>
         </section>
       </main>
